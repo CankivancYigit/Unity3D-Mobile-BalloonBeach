bb7143be4041b7cae125d9086ae190014d6b5f43
CankivancYigit <CankivancYigit@github.com>
2021-02-01 06:30:03 +0300

    Camera Follow Script Added To Follow The Player.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [Tooltip("Distance Between Camera And Player")][SerializeField] float distanceCameraPlayer = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, transform.position.y, player.transform.position.z - distanceCameraPlayer), Time.deltaTime * 100f); 
+    }
+}
