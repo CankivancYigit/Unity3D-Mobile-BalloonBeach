6d3662136f5ae1563f7b9d5d481ce5c97cd8c426
CankivancYigit <CankivancYigit@github.com>
2021-02-01 07:55:10 +0300

    Triangle Obstacles Are Created And Score System Added.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+    int score = 0;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "ScoreUp")
+        {
+            score++;
+        }
+    }
+}
