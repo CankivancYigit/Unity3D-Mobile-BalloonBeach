7b6e52ec58489ceeaf7bc1c731397185fef1b3d6
CankivancYigit <CankivancYigit@github.com>
2021-02-02 07:15:39 +0300

    World Shader And Infinite Level Generation Added.

---
+++ Assets/Scripts/AppInitialize.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppInitialize : MonoBehaviour
+{
+    private void Awake()
+    {
+        Shader.SetGlobalFloat("_Curvature", 2.0f);
+        Shader.SetGlobalFloat("_Trimming", 0.1f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine("Transfer");
+    }
+
+    IEnumerator Transfer()
+    {
+        yield return new WaitForSeconds(1);
+        gameObject.transform.parent.position = new Vector3(0, 0, gameObject.transform.position.z + 200);
+    }
+}
