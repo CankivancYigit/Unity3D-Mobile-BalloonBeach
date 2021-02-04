0da44b4ed20dab8ef9cc7718c88076f70831625e
CankivancYigit <CankivancYigit@github.com>
2021-02-04 13:26:13 +0300

    Score Saving System Added.

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,12 +6,26 @@ using TMPro;
 public class Score : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI highScoreText;
     int score = 0;
-   
+    int highScore;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("highscore");
+    }
+
     // Update is called once per frame
     void Update()
     {
         scoreText.text = score.ToString();
+        highScoreText.text = highScore.ToString();
+        if (score>highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("highscore", score);
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
